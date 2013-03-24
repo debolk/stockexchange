a3995246c0ccea655cecca8086f557018b9c6f50
Jakob Buis <jakob@cubiso.nl>
2013-03-24 18:07:22 +0100

    added change action for matched orders

--- public/javascripts/buy_booth.js
+++ public/javascripts/buy_booth.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   // Compile templates
-  var order_row = Handlebars.compile($("#order_row").html());
+  var open_order = Handlebars.compile($("#open_order").html());
+  var matched_order = Handlebars.compile($("#matched_order").html());
 
   // Load commodities
   $.getJSON('/commodities', function(commodities) {
@@ -12,12 +13,11 @@ $(document).ready(function(){
   // Load orders
   $.getJSON('/buy_orders', function(buy_orders) {
     $(buy_orders).each(function() {
-      var row = order_row(this);
       if (this.state == 'matched') {
-        $('#matched_orders tbody').append(row);
+        $('#matched_orders tbody').append(matched_order(this));
       }
       else {
-        $('#open_orders tbody').append(row);
+        $('#open_orders tbody').append(open_order(this));
       }
     });
   });
@@ -26,5 +26,24 @@ $(document).ready(function(){
   $('input.all').on('click', function(){
     var table = $(this).parents('table');
     $('tbody input[type="checkbox"]', table).prop('checked', $(this).is(':checked'));
+  });
+
+  // Confirm action
+  $('#matched_orders').on('click', '.confirm', function(event){
+    event.preventDefault();
+    var row = $(this).parents('tr');
+    $.ajax({
+      type: 'PUT',
+      url: '/buy_orders/'+row.attr('data-id')+'/payment?214E7DD41B7C823DF963',
+      contentType: 'application/json',
+      success: function(result){
+        row.remove();
+        message = $('<div>').addClass('alert alert-success').html('Order confirmed');
+        $('#matched_orders').before(message);
+      },
+      error: function() {
+        alert('Something went wrong. Please try again or reload');
+      },
+    });
   });
 });
