e6758e446c7e4511e3060f2e6c69db6567a66733
Jakob Buis <jakob@cubiso.nl>
2013-04-07 17:28:10 +0200

    market has an heartbeat

--- market.rb
+++ market.rb
@@ -4,3 +4,23 @@ require './setup'
 print 'Starting market interface servers...'
 system 'thin start > /dev/null &'
 puts 'done'
+
+print 'Loading market software...'
+class Market
+end
+market = Market.new
+puts 'done'
+
+print 'Initializing market heartbeat...'
+threads = []
+threads << Thread.new do
+  while true
+    puts 'beep'
+    sleep 1
+  end
+end
+puts 'done'
+
+puts 'GONG! Markets are open'
+puts ''
+threads.each { |thr| thr.join }
