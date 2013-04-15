3f9a1d45e87a3c6bb65677bd4551b464b101501e
Jakob Buis <jakob@cubiso.nl>
2013-04-15 17:24:56 +0200

    improvements to barman-interface

--- public/javascripts/barman.js
+++ public/javascripts/barman.js
@@ -7,11 +7,62 @@ $(document).ready(function(){
     $(commodities).each(function(){
       $('table tbody').append(commodity(this));
     });
+    // Append index numbers
+    $('tbody .index').each(function(index){
+      $(this).text('F'+(index+1));
+    });
   });
 
   // Calculate total
   calculate_total();
   $('table').on('keyup', '.amount', calculate_total);
+
+  // Key handlers
+  $(document).on('keyup', function(event){
+    event.preventDefault();
+    switch (event.keyCode)
+    {
+      // Switch to a different field
+      case 112: focus_field(0); break; // F1
+      case 113: focus_field(1); break; // F2
+      case 114: focus_field(2); break; // F3
+      case 115: focus_field(3); break; // F4
+      // Submit
+      case 120: $('form').submit(); break; // F9
+    }
+  });
+
+  // Submit form
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    alert('To be implemented!');
+    // var data = {
+    //   commodity: $('[name="commodity"]').val(),
+    //   amount: $('[name="amount"]').val(),
+    //   price: $('[name="price"]').val(),
+    //   phone: $('[name="phone"]').val(),
+    // }
+    // $.ajax({
+    //   method: 'POST',
+    //   url: '/buy_orders?214E7DD41B7C823DF963', 
+    //   data: JSON.stringify(data), 
+    //   success: function(result) {
+    //     StockExchange.addAlert('success', 'Buy order added', true);
+    //     $('#open_orders tbody').append(open_order(result)); // Append to body
+    //   },
+    //   contentType: 'json',
+    //   dataType: 'json',
+    // });
+  });
+
+  function focus_field(focus_index)
+  {
+    $('tbody input[type="number"]').each(function(field_index){
+      if (focus_index === field_index) {
+        $(this).focus();
+      }
+    });
+  }
 
   function calculate_total()
   {
