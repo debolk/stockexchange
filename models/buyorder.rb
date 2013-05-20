15efdff8bd7bdfcb3209e0653f5a69990e36dca7
Jakob Buis <jpabuis@gmail.com>
2013-05-20 15:46:52 +0200

    implemented maximum limit to the order book

--- models/buyorder.rb
+++ models/buyorder.rb
@@ -9,11 +9,21 @@ class BuyOrder < ActiveRecord::Base
   validates :commodity, presence: true
   validates_uniqueness_of :commodity_id, :scope => [:phone, :state, :deleted_at], :unless => Proc.new {|bo| bo.deleted?}, :if => Proc.new {|bo| bo.state == 'open'}
 
+  after_create :remove_lowest_order
+
   def commodity_name
     commodity.name
   end
 
   def total_value
     price*amount
+  end
+
+  private
+
+  def remove_lowest_order
+    if BuyOrder.count > 10
+      BuyOrder.order('price asc').limit(1).destroy_all
+    end
   end
 end
