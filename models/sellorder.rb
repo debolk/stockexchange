1e1f8efb0b5cc570d4f671f9e735332c2803ab40
Jakob Buis <jpabuis@gmail.com>
2013-05-23 16:34:10 +0200

    also remove sell orders

--- models/sellorder.rb
+++ models/sellorder.rb
@@ -5,4 +5,17 @@ class SellOrder < ActiveRecord::Base
 
   validates :price, presence: true, numericality: true
   validates :commodity, presence: true
+
+  after_create :remove_lowest_order
+
+  private
+
+  def remove_lowest_order
+    Commodity.all.each do |commodity|
+      to_be_destroyed = commodity.buy_orders.count - 10
+      if to_be_destroyed > 0
+        commodity.buy_orders.order('price asc').limit(to_be_destroyed).destroy_all
+      end
+    end
+  end
 end
